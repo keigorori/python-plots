04f09ad32087fd7f4266b48e87f9003dbba74777
keigorori <www.k544.com@gmail.com>
2019-08-16 13:00:21 +0900

    最低限のsin波プロット処理を実装

---
+++ plot-sin.py
@@ -0,0 +1,15 @@
+﻿# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    x = np.linspace(0, 50, 1000)
+    y = np.sin(x)
+    plt.plot(x,y)
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()
