1314a0a4e06969d5b252b1a01b4473d18df57a87
keigorori <www.k544.com@gmail.com>
2022-11-25 19:07:31 +0900

    3dプロットのサンプルを追加

---
+++ plot-3d.py
@@ -0,0 +1,21 @@
+﻿# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+def main():
+    x = np.linspace(-10, 10, 512)
+    y = np.linspace(-10, 10, 512)
+    x, y = np.meshgrid(x, y)
+    z = np.sin(np.sqrt(x**2+y**2)) / np.sqrt(x**2+y**2)
+
+    # plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_wireframe(x, y, z)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
